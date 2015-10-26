ab93ad7831f5741b4252415a39080cbf623b9bbf
Michael Long <long1962@bellsouth.net>
2015-10-26 17:01:13 -0400

    index.php file

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include('inc/head.php'); ?>
+   
+        <div id="container">
+            <?php include('inc/header.php'); ?>
+   
+            <div id="body">
+                <div class="main">
+                    <a class="btn-main" href="login.php">login</a> 
+                </div>
+                    <img src="img/EHS_Graphic1.jpg">
+            </div> <!-- id="body" -->
+       
+            <?php  
+                include('inc/footer.php');
+            ?>
+    
+        </div> <!--id="Container" -->
+    </body>
+</html>
